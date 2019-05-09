286df06de0cfc9b76e94df7376d7a24a598ddf67
Quintin Henn <quintin.henn@gmail.com>
2019-05-09 04:05:17 +0200

    Removed dependencies from the repository

---
+++ app/assets/js/general.js
@@ -0,0 +1,15 @@
+ function padzero(n) {
+      return n < 10 ? '0' + n : n;
+  }
+ function pad2zeros(n) {
+      if (n < 100) {
+          n = '0' + n;
+      }
+      if (n < 10) {
+          n = '0' + n;
+     }
+     return n;
+ }
+ function toISOString(d) {
+     return d.getUTCFullYear() + '-' +  padzero(d.getUTCMonth() + 1) + '-' + padzero(d.getUTCDate()+1) + 'T' + padzero(d.getUTCHours()) + ':' +  padzero(d.getUTCMinutes()) + ':' + padzero(d.getUTCSeconds()) + '.' + pad2zeros(d.getUTCMilliseconds()) + 'Z';
+ }
